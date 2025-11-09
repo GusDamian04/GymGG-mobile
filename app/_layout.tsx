b7b9adf06fbfda64fe8f70f30e8bae64573f7024
GusDamian <damiangustavoangel@gmail.com>
2025-11-08 18:56:27 -0600

    Create index page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,9 +14,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* Se define que el login sea la pantalla principal */}
+        <Stack.Screen name="login" />
+        
+        {/* Se define que el resto de pantallas sean las de la app */}
+        <Stack.Screen name="(tabs)" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
